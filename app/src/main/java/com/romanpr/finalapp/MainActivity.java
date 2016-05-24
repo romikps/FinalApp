bdebfb5dedfed241bb459f05a6c7b613f7a03702
romikps <roman.priscepov@hotmail.com>
2016-05-24 10:03:51 +0300

    Working location updates.

--- app/src/main/java/com/romanpr/finalapp/MainActivity.java
+++ app/src/main/java/com/romanpr/finalapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.romanpr.finalapp;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     LocationManager locationManager;
     Location mCurrentLocation;
+    LocationListener locationListener;
+    double findLatitude, findLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,12 @@ public class MainActivity extends AppCompatActivity {
         myRef = database.getReference("users");
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
+        locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 mCurrentLocation = location;
                 logLocation();
+                processUser("Roman", mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -41,7 +49,27 @@ public class MainActivity extends AppCompatActivity {
 
             public void onProviderDisabled(String provider) {}
         };
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        locationManager.requestLocationUpdates(locationManager.getBestProvider(new Criteria(), false),
+                10000, 5, locationListener);
+
+    }
+
+    public void updateDistance(View view) {
+        myRef.child("Roman").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                findLatitude = Double.valueOf(dataSnapshot.child("latitude").getValue().toString());
+                findLongitude = Double.valueOf(dataSnapshot.child("longitude").getValue().toString());
+                Log.i("Location", "Find: " + findLatitude + ", " + findLongitude);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
